42f6f163ad331dff1b3fa45e7640a6e9c2b20c2e
superlucky84 <superlucky84@gmail.com>
2023-07-14 00:48:41 +0900

    lithent verionup 1.0.5 apply

--- src/tmpl/headerTmpl.js
+++ src/tmpl/headerTmpl.js
@@ -2,6 +2,34 @@ import { h, mount, Fragment } from 'lithent';
 import htm from 'htm';
 const html = htm.bind(h);
 
+const DefaultHeader = mount(() => {
+  return ({
+    events,
+    showJumpButtons,
+    isDateCalendar,
+    isWeekCalendar,
+    titleClass,
+    title,
+    type,
+  }) => html`
+    <div>
+      ${!isWeekCalendar &&
+      html`<button onClick=${events.drawPrevYear}>Prev year</button>`}
+      ${isDateCalendar &&
+      html`<button onClick=${events.drawPrevMonth}>Prev month</button>`}
+      ${isWeekCalendar &&
+      html`<button onClick=${events.drawPrevWeek}>Prev week</button>`}
+      <em>${title}</em>
+      ${isWeekCalendar &&
+      html`<button onClick=${events.drawNextWeek}>Next week</button>`}
+      ${isDateCalendar &&
+      html`<button onClick=${events.drawNextMonth}>Next month</button>`}
+      ${!isWeekCalendar &&
+      html`<button onClick=${events.drawNextYear}>Next year</button>`}
+    </div>
+  `;
+});
+
 export const HeaderTmpl = mount(
   (
     renew,
@@ -31,7 +59,7 @@ export const HeaderTmpl = mount(
 
     return () => html`
       <>
-        <${customTmpl}
+        <${customTmpl || DefaultHeader}
           events=${events}
           showJumpButtons=${showJumpButtons}
           isDateCalendar=${isDateCalendar}
