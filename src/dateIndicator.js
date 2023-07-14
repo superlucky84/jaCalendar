7cc369e2112017ef61e5ca6c03523acdf350e29d
superlucky84 <superlucky84@gmail.com>
2023-07-14 13:21:37 +0900

    feat: 템플레이트 옵션을 최종 사용자가 직접 넣을스 있도록 위치 이동

--- src/dateIndicator.js
+++ src/dateIndicator.js
@@ -1,149 +1,4 @@
-import { Calendar, CLASS_NAME_HEADER, CLASS_NAME_BODY } from '@/calendar';
-import { dateUtil } from '@/helper/dateUtil';
-
-import { h, mount } from 'lithent';
-import htm from 'htm';
-const html = htm.bind(h);
-
-export const LayoutTmpl = mount(
-  () => () =>
-    html`
-      <div>
-        asdga
-        <div class=${CLASS_NAME_HEADER}></div>
-        <div class=${CLASS_NAME_BODY}></div>
-        vase
-      </div>
-    `
-);
-
-export const HeaderTmpl = mount(() => {
-  return ({
-    events,
-    showJumpButtons,
-    isDateCalendar,
-    isWeekCalendar,
-    titleClass,
-    title,
-    type,
-  }) => html`
-    <div>
-      ${!isWeekCalendar &&
-      html`<button onClick=${events.drawPrevYear}>Prev year</button>`}
-      ${isDateCalendar &&
-      html`<button onClick=${events.drawPrevMonth}>Prev month</button>`}
-      ${isWeekCalendar &&
-      html`<button onClick=${events.drawPrevWeek}>Prev week</button>`}
-      <em>${title}</em>
-      ${isWeekCalendar &&
-      html`<button onClick=${events.drawNextWeek}>Next week</button>`}
-      ${isDateCalendar &&
-      html`<button onClick=${events.drawNextMonth}>Next month</button>`}
-      ${!isWeekCalendar &&
-      html`<button onClick=${events.drawNextYear}>Next year</button>`}
-    </div>
-  `;
-});
-
-export const BodyWeekTmpl = mount(() => {
-  return ({ Sun, Mon, Tue, Wed, Thu, Fri, Sat, weeks, customOptions }) =>
-    html`
-      <table cellspacing="0" cellpadding="0">
-        <caption>
-          <span>Date!!!s</span>
-        </caption>
-        <tr>
-          ${weeks.map(item => html` <td>${item.dayInMonth}</td> `)}
-        </tr>
-        <tr>
-          <th scope="col">${Sun}</th>
-          <th scope="col">${Mon}</th>
-          <th scope="col">${Tue}</th>
-          <th scope="col">${Wed}</th>
-          <th scope="col">${Thu}</th>
-          <th scope="col">${Fri}</th>
-          <th scope="col">${Sat}</th>
-        </tr>
-        <tr>
-          ${weeks.map(
-            item =>
-              html`
-                <td
-                  class=${getTargetClass(
-                    customOptions,
-                    new Date(item.timestamp),
-                    'week'
-                  )}
-                  onClick=${() => console.log(new Date(item.timestamp))}
-                  data-timestamp=${item.timestamp}
-                >
-                  ${item.dayInMonth}
-                </td>
-              `
-          )}
-        </tr>
-      </table>
-    `;
-});
-
-const getTargetClass = (infos, targetDate, type) => {
-  return Object.entries(infos)
-    .reduce((acc, [key, value]) => {
-      if (dateUtil.isSame(targetDate, new Date(Number(key)), type)) {
-        acc = acc.concat(value);
-      }
-
-      return acc;
-    }, [])
-    .join(' ');
-};
-
-export const BodyDateTmpl = mount(() => {
-  return ({ Sun, Mon, Tue, Wed, Thu, Fri, Sat, weeks, customOptions }) =>
-    html`
-      <table cellspacing="0" cellpadding="0">
-        <caption>
-          <span>Dates</span>
-        </caption>
-        <thead>
-          <tr>
-            <th scope="col">${Sun}</th>
-            <th scope="col">${Mon}</th>
-            <th scope="col">${Tue}</th>
-            <th scope="col">${Wed}</th>
-            <th scope="col">${Thu}</th>
-            <th scope="col">${Fri}</th>
-            <th scope="col">${Sat}</th>
-          </tr>
-        </thead>
-        <tbody>
-          ${weeks.map(
-            week => html`
-              <tr>
-                ${week.map(
-                  item =>
-                    html`
-                      <td
-                        class=${getTargetClass(
-                          customOptions,
-                          new Date(item.timestamp),
-                          'date'
-                        )}
-                        onClick=${() => console.log(new Date(item.timestamp))}
-                        data-timestamp=${item.timestamp}
-                      >
-                        ${item.dayInMonth}
-                      </td>
-                    `
-                )}
-              </tr>
-            `
-          )}
-        </tbody>
-      </table>
-    `;
-});
-
+import { Calendar } from '@/calendar';
 export class DateIndicator {
   constructor({
     type,
@@ -153,6 +8,12 @@ export class DateIndicator {
     weekStartStandardDay,
     showJumpButtons,
     language,
+    layoutTmpl,
+    headerTmpl,
+    bodyYearTmpl,
+    bodyMonthTmpl,
+    bodyWeekTmpl,
+    bodyDateTmpl,
     customOptions,
   }) {
     this.type = type;
@@ -165,12 +26,12 @@ export class DateIndicator {
       weekStartStandardDay,
       showJumpButtons,
       type,
-      layoutTmpl: LayoutTmpl,
-      headerTmpl: HeaderTmpl,
-      bodyYearTmpl: null,
-      bodyMonthTmpl: null,
-      bodyWeekTmpl: BodyWeekTmpl,
-      bodyDateTmpl: BodyDateTmpl,
+      layoutTmpl,
+      headerTmpl,
+      bodyYearTmpl,
+      bodyMonthTmpl,
+      bodyWeekTmpl,
+      bodyDateTmpl,
       customOptions,
     });
   }
--- src/example/index.js
+++ src/example/index.js
@@ -1,4 +1,148 @@
+import { CLASS_NAME_HEADER, CLASS_NAME_BODY } from '@/calendar';
 import { DateIndicator } from '@/dateIndicator';
+import { dateUtil } from '@/helper/dateUtil';
+import { h, mount } from 'lithent';
+import htm from 'htm';
+const html = htm.bind(h);
+
+export const LayoutTmpl = mount(
+  () => () =>
+    html`
+      <div>
+        asdga
+        <div class=${CLASS_NAME_HEADER}></div>
+        <div class=${CLASS_NAME_BODY}></div>
+        vase
+      </div>
+    `
+);
+
+export const HeaderTmpl = mount(() => {
+  return ({
+    events,
+    showJumpButtons,
+    isDateCalendar,
+    isWeekCalendar,
+    titleClass,
+    title,
+    type,
+  }) => html`
+    <div>
+      ${!isWeekCalendar &&
+      html`<button onClick=${events.drawPrevYear}>Prev year</button>`}
+      ${isDateCalendar &&
+      html`<button onClick=${events.drawPrevMonth}>Prev month</button>`}
+      ${isWeekCalendar &&
+      html`<button onClick=${events.drawPrevWeek}>Prev week</button>`}
+      <em>${title}</em>
+      ${isWeekCalendar &&
+      html`<button onClick=${events.drawNextWeek}>Next week</button>`}
+      ${isDateCalendar &&
+      html`<button onClick=${events.drawNextMonth}>Next month</button>`}
+      ${!isWeekCalendar &&
+      html`<button onClick=${events.drawNextYear}>Next year</button>`}
+    </div>
+  `;
+});
+
+export const BodyWeekTmpl = mount(() => {
+  return ({ Sun, Mon, Tue, Wed, Thu, Fri, Sat, weeks, customOptions }) =>
+    html`
+      <table cellspacing="0" cellpadding="0">
+        <caption>
+          <span>Date!!!s</span>
+        </caption>
+        <tr>
+          ${weeks.map(item => html` <td>${item.dayInMonth}</td> `)}
+        </tr>
+        <tr>
+          <th scope="col">${Sun}</th>
+          <th scope="col">${Mon}</th>
+          <th scope="col">${Tue}</th>
+          <th scope="col">${Wed}</th>
+          <th scope="col">${Thu}</th>
+          <th scope="col">${Fri}</th>
+          <th scope="col">${Sat}</th>
+        </tr>
+        <tr>
+          ${weeks.map(
+            item =>
+              html`
+                <td
+                  class=${getTargetClass(
+                    customOptions,
+                    new Date(item.timestamp),
+                    'week'
+                  )}
+                  onClick=${() => console.log(new Date(item.timestamp))}
+                  data-timestamp=${item.timestamp}
+                >
+                  ${item.dayInMonth}
+                </td>
+              `
+          )}
+        </tr>
+      </table>
+    `;
+});
+
+const getTargetClass = (infos, targetDate, type) => {
+  return Object.entries(infos)
+    .reduce((acc, [key, value]) => {
+      if (dateUtil.isSame(targetDate, new Date(Number(key)), type)) {
+        acc = acc.concat(value);
+      }
+
+      return acc;
+    }, [])
+    .join(' ');
+};
+
+export const BodyDateTmpl = mount(() => {
+  return ({ Sun, Mon, Tue, Wed, Thu, Fri, Sat, weeks, customOptions }) =>
+    html`
+      <table cellspacing="0" cellpadding="0">
+        <caption>
+          <span>Dates</span>
+        </caption>
+        <thead>
+          <tr>
+            <th scope="col">${Sun}</th>
+            <th scope="col">${Mon}</th>
+            <th scope="col">${Tue}</th>
+            <th scope="col">${Wed}</th>
+            <th scope="col">${Thu}</th>
+            <th scope="col">${Fri}</th>
+            <th scope="col">${Sat}</th>
+          </tr>
+        </thead>
+        <tbody>
+          ${weeks.map(
+            week => html`
+              <tr>
+                ${week.map(
+                  item =>
+                    html`
+                      <td
+                        class=${getTargetClass(
+                          customOptions,
+                          new Date(item.timestamp),
+                          'date'
+                        )}
+                        onClick=${() => console.log(new Date(item.timestamp))}
+                        data-timestamp=${item.timestamp}
+                      >
+                        ${item.dayInMonth}
+                      </td>
+                    `
+                )}
+              </tr>
+            `
+          )}
+        </tbody>
+      </table>
+    `;
+});
 
 const ins = new DateIndicator({
   container: document.getElementById('calendar-wrapper'),
@@ -8,6 +152,12 @@ const ins = new DateIndicator({
   weekStartDay: 'sun',
   weekStartStandardDay: 'thu',
   showJumpButtons: true,
+  layoutTmpl: LayoutTmpl,
+  headerTmpl: HeaderTmpl,
+  bodyYearTmpl: null,
+  bodyMonthTmpl: null,
+  bodyWeekTmpl: BodyWeekTmpl,
+  bodyDateTmpl: BodyDateTmpl,
   customOptions: {
     1689300057808: ['classA', 'classB', 'classC'],
     1689131591000: ['classA', 'classB'],
